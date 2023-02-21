435cdb7eb6a62297417896beffcda1a84a92f3cd
Roman Borsoev <64916155+borsoy@users.noreply.github.com>
2023-02-21 14:42:38 +0800

    Git init

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает
+//два числа и выдаёт, какое число большее, а какое меньшее.
+// Пример:
+// a = 5; b = 7 -> max = 7, min = 5
+// a = -9; b = -3 - max = -3, min = -9
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+    Console.WriteLine($"a = {number1}, b = {number2} -> max = {number1}, min = {number2}");
+else
+{
+    Console.WriteLine($"a = {number1}, b = {number2} -> max = {number2}, min = {number2}");
+}
---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает маскиально из этих чисел.
+// Пример:
+// 2,3,7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+Console.Write("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max_number = 0;
+if (number1 > number2)
+        max_number = number1;
+        else
+        {
+            max_number = number2;
+        
+        }
+if (number2 > number3)
+        max_number = number2;
+        else
+        {
+            max_number = number3;
+
+        }
+
+Console.Write($"{number1}, {number2}, {number3} -> {max_number}");
